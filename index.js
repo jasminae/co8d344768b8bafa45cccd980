3724704ee75a38df8c6166d4cfa69be9a95e26ce
jasminae <jas-mina@online.no>
2022-12-19 21:58:01 +0100

    changed js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/**
-Challenge: 
-
-- Make the styling more exciting once an activity idea comes 
-back from the Bored API
-    - Resources: DOM element "classList" property, uigradients.com, 
-      Google Fonts, color.adobe.com
-    - Some ideas:
-      - Change the title from "BoredBot" to something more exciting!
-      - Change the background to something less drab.
-      - Bonus: Animate something on the screen to move around and add more 
-        excitement to the page
-*/
-
 function myFunction() {
 
   fetch("https://some-random-api.ml/facts/cat")
